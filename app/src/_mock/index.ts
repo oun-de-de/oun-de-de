2d421e23e4d35d19904df5342befa7777959dcfb
Nguyễn Hiếu Kiên <denwingsa01884@gmail.com>
2026-01-14 22:59:10 +0700

    implement dashboard mockup

--- app/src/_mock/index.ts
+++ app/src/_mock/index.ts
@@ -1,9 +1,10 @@
 import { setupWorker } from "msw/browser";
 import { mockTokenExpired } from "./handlers/_demo";
+import { dailyIncomePos, customerInfo, performance } from "./handlers/_dashboard";
 import { menuList } from "./handlers/_menu";
 import { signIn, userList } from "./handlers/_user";
 
-const handlers = [signIn, userList, mockTokenExpired, menuList];
+const handlers = [signIn, userList, mockTokenExpired, menuList, dailyIncomePos, customerInfo, performance];
 const worker = setupWorker(...handlers);
 
 export { worker };
